736ee45b1967b71fe8e348de43e2e8c385e2328f
kegs1287 <bdkagel@ilstu.edu>
2025-02-11 15:51:44 -0600

    Add files via upload

---
+++ welcome2.js
@@ -0,0 +1,27 @@
+let ufo = {x: 50, y: 80, width: 80, speed: 1}
+
+
+function draw() {
+  drawUfo()
+  moveUfo()
+}
+
+
+function drawUfo() {
+  fill("silver")
+  ellipse(ufo.x, ufo.y, ufo.width, 20)
+  ellipse(ufo.x, ufo.y - 10, ufo.width / 2, 25)
+}
+
+function moveUfo() {
+  ufo.x += ufo.speed
+  if (ufo.x > width || ufo.x < 0) {
+    ufo.speed = -ufo.speed
+  }
+}
+
+function random(min, max) {
+    return min + Math.random() * (max + 1 - min);
+  }
+
+  
